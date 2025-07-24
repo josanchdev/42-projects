cf843b93f0754820f9856eb9579463f6d28eb2eb
Jorge Sanchez Aguilar <josanch2@c1r11s4.42madrid.com>
2025-07-24 12:49:37 +0200

    Added libft

---
+++ libft/ft_calloc.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: josanch2 <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/26 17:19:05 by josanch2          #+#    #+#             */
+/*   Updated: 2023/09/26 17:19:23 by josanch2         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_calloc(size_t count, size_t size)
+{
+	void	*ptr;
+
+	ptr = (char *)malloc(size * count);
+	if (!ptr)
+		return (NULL);
+	ft_bzero (ptr, count * size);
+	return (ptr);
+}
